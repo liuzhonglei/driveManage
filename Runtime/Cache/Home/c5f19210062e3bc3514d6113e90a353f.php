4c0d84db35e562ff9e30e9d9d14d634e831bc7df
wuhanchu <118130760@qq.com>
2015-07-09 17:18:41 +0800

    New changelist

--- Runtime/Cache/Home/c5f19210062e3bc3514d6113e90a353f.php
+++
@@ -1,9 +0,0 @@
-<?php if (!defined('THINK_PATH')) exit();?><link rel="stylesheet" href="/Addons/ReturnTop/css/0<?php echo ($addons_config["current"]); ?>.css">
-<style>
-	#topcontrol a.top_stick{
-		display: block;
-		outline: medium none;
-		text-decoration: none;
-	}
-</style>
-<script src="/Addons/ReturnTop/scrolltopcontrol.js"></script>
