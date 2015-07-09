071870e76eea3962179e12fd50fda9b630182d79
wuhanchu <118130760@qq.com>
2015-07-09 17:29:34 +0800

    修改相关数据。

---
+++ Addons/QingQing/Controller/QingQingController.class.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Addons\QingQing\Controller;
+
+use Home\Controller\AddonsController;
+
+
+/**
+ * Class QingQingController platform controller
+ * @package Addons\QingQing\Controller
+ */
+class QingQingController extends AddonsController
+{
+    /**
+     * show the main view
+     */
+    function show()
+    {
+        // display
+    }
+}
