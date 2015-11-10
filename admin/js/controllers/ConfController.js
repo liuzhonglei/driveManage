dba3fee2c3d3371b2d73323aeba941af218e3a2a
wuhanchu <118130760@qq.com>
2015-11-10 18:18:45 +0800

    ADMIN前台代码更新。

--- admin/js/controllers/ConfController.js
+++ admin/js/controllers/ConfController.js
@@ -1,38 +1,14 @@
 /* Setup general page controller */
-MetronicApp.controller('ListController', ['$rootScope', '$http', '$scope', function ($rootScope, $http, $scope) {
-    // param
-    $scope.stateData = $rootScope.$state.$current.data;
-
-    // init ajax
-    $scope.$on('$viewContentLoaded', function () {
-        //init
-        Metronic.initAjax();
-
-        // init the table
-        TableAjax.init($rootScope.$state.$current.data.module, $rootScope.$state.$current.data.handleController);
-        $scope.TableAjax = TableAjax;
-
-        // init the list
-        $http({
-            method: "post",
-            headers: function ($httpProvider) {
-                $httpProvider.defaults.withCredentials = true;
-            },
-            url: Metronic.rootPath() + "/index.php?s=/addon/"+$rootScope.$state.$current.data.module+"/"+$rootScope.$state.$current.data.handleController+"/getModelInfo"
-        }).success(function (data) {
-            $scope.searchName = data.model.search_key;
-        });
-
-        // init the config
-        $http({
-            method: "get",
-            headers: function ($httpProvider) {
-                $httpProvider.defaults.withCredentials = true;
-            },
-            url: Metronic.rootPath() + "/index.php?s=/addon/"+$rootScope.$state.$current.data.module+"/"+$rootScope.$state.$current.data.handleController+"/configAdmin"
-        }).success(function (data) {
-            $scope.addon = data;
-        });
+MetronicApp.controller('ConfController', ['$rootScope', '$http', '$scope', function ($rootScope, $http, $scope) {
+    // init the config
+    $http({
+        method: "get",
+        headers: function ($httpProvider) {
+            $httpProvider.defaults.withCredentials = true;
+        },
+        url: Metronic.rootPath() + "/index.php?s=/addon/" + $rootScope.$state.$current.data.module + "/" + $rootScope.$state.$current.data.handleController + "/configAdmin"
+    }).success(function (data) {
+        $scope.addon = data;
     });
 
     /**
@@ -41,25 +17,31 @@ MetronicApp.controller('ListController', ['$rootScope', '$http', '$scope', funct
     $scope.saveConf = function () {
         var config = {};
         //var params = JSON.stringify(config).replace("{", "").replace("}", "").replace(/\,/g, "&").replace(/\"/g, "").replace(/\:/g, "=");
-        for(var key in $scope.addon.config){
+        for (var key in $scope.addon.config) {
             config[key] = $scope.addon.config[key].value;
         }
         $http({
             method: "post",
-            url: Metronic.rootPath() + "/index.php?s=/addon/"+$rootScope.$state.$current.data.module+"/"+$rootScope.$state.$current.data.handleController+"/configAdmin/",
+            url: Metronic.rootPath() + "/index.php?s=/addon/" + $rootScope.$state.$current.data.module + "/" + $rootScope.$state.$current.data.handleController + "/configAdmin/",
             data: config,
             headers: {
                 'Content-Type': 'application/json'
             }
         }).success(function (data) {
-            if (data.status == "1") {
-                $scope.infoErrorShow = false;
-                $scope.infoErrorMsg = "";
-                TableAjax.reload();
-                $("#form_info").modal("hide");
+            $scope.showMsg = data.msg;
+
+            if (data.status == "0") {
+                $scope.errorShow = true;
+                $scope.successShow = false;
+                if($scope.showMsg == null || $scope.showMsg.length < 1){
+                    $scope.showMsg = "操作失败！";
+                }
             } else {
-                $scope.infoErrorShow = true;
-                $scope.infoErrorMsg = data.msg;
+                $scope.errorShow = false;
+                $scope.successShow = true;
+                if($scope.showMsg == null || $scope.showMsg.length < 1){
+                    $scope.showMsg = "操作成功！";
+                }
             }
         });
     };
---
+++ admin/js/scripts/info.js
@@ -0,0 +1,4 @@
+//var fileList = new Array('../assets/admin/pages/scripts/components-pickers.js');
+//for(var file in fileList){
+//    document.write('<script type=\\"text/javascript\\" src=\\"' + file + '\\"></sc' + 'ript>');
+//}
