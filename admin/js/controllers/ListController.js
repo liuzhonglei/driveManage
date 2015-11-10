a418caa29625ced8d5bd4605cb50743a094e7098
wuhanchu <118130760@qq.com>
2015-11-10 18:19:40 +0800

    修改使用教练微信号进行学员注册的情况。

--- admin/js/controllers/ListController.js
+++ admin/js/controllers/ListController.js
@@ -5,7 +5,6 @@ MetronicApp.filter('propsFilter', function () {
         if (angular.isArray(items)) {
             items.forEach(function (item) {
                 var itemMatches = false;
-
                 var keys = Object.keys(props);
                 for (var i = 0; i < keys.length; i++) {
                     var prop = keys[i];
@@ -21,7 +20,6 @@ MetronicApp.filter('propsFilter', function () {
                 }
             });
         } else {
-            // Let the output be the input untouched
             out = items;
         }
 
@@ -30,169 +28,29 @@ MetronicApp.filter('propsFilter', function () {
 });
 
 /* Setup general page controller */
-MetronicApp.controller('ListController', ['$rootScope', '$http', '$scope', '$timeout', 'metadata', function ($rootScope, $http, $scope, $timeout, metadata) {
+MetronicApp.controller('ListController', ['$rootScope', '$http', '$scope', function ($rootScope, $http, $scope) {
     // param
-    $scope.model = $rootScope.$state.$current.data.model;
-    $scope.metadata = {};
-    $scope.selected = {};
-    $scope.fieldList = {};
-    $scope.info = {};
-    $scope.infoId = null;
+    $scope.rootData = $rootScope.$state.$current.data;
 
     // init ajax
     $scope.$on('$viewContentLoaded', function () {
-        // init
-        ;(function($){
-            $.fn.datepicker.dates['zh-CN'] = {
-                days: ["星期日", "星期一", "星期二", "星期三", "星期四", "星期五", "星期六", "星期日"],
-                daysShort: ["周日", "周一", "周二", "周三", "周四", "周五", "周六", "周日"],
-                daysMin:  ["日", "一", "二", "三", "四", "五", "六", "日"],
-                months: ["一月", "二月", "三月", "四月", "五月", "六月", "七月", "八月", "九月", "十月", "十一月", "十二月"],
-                monthsShort: ["一月", "二月", "三月", "四月", "五月", "六月", "七月", "八月", "九月", "十月", "十一月", "十二月"],
-                today: "今日",
-                format: "yyyy年mm月dd日",
-                weekStart: 1
-            };
-        }(jQuery));
-
         //init
         Metronic.initAjax();
 
         // init the table
-        TableAjax.init($rootScope.$state.$current.data.model);
+        TableAjax.init($rootScope.$state.$current.data.module, $rootScope.$state.$current.data.handleController);
         $scope.TableAjax = TableAjax;
-        // init the info
+
+        // init the field
         $http({
             method: "post",
             headers: function ($httpProvider) {
                 $httpProvider.defaults.withCredentials = true;
             },
-            url: Metronic.rootPath() + "/index.php?s=/addon/School/Admin/getFieldList/model/" + $scope.model
+            url: Metronic.rootPath() + "/index.php?s=/addon/" + $rootScope.$state.$current.data.module + "/" + $rootScope.$state.$current.data.handleController + "/getModelInfo"
         }).success(function (data) {
-            // init the metadata
-            for (var i in data) {
-                for (var j in data[i]) {
-                    if (data[i][j] !== null) {
-                        var extra = data[i][j].extra;
-                        if (extra !== undefined && extra !== null && extra.length > 0) {
-                            var result = [];
-                            var params = extra.split("\r\n");
-                            for (var z in params) {
-                                var map = params[z].split(":");
-                                if (map.length == 2) {
-                                    result.push({"value": map[0], "text": map[1]});
-                                }
-                            }
-                            $scope.metadata[data[i][j].name] = result;
-                        }
-                    }
-                }
-            }
-
-            // set  the fieldList
-            $scope.fieldList = data[1];
-            $scope.search_field = data[1];
-
-
+            // set searchTexg
+            $scope.searchName = data.model.search_key;
         });
     });
-
-    // get edit info
-
-    $scope.$watch('infoId', function () {
-        $(".fileinput").fileinput('clear');
-
-        if ($scope.infoId != null && $scope.infoId != "") {
-            $http({
-                method: "get",
-                url: Metronic.rootPath() + "/index.php?s=/addon/School/Admin/getDataById/model/" + $scope.model + "/id/" + $scope.infoId
-            }).success(function (data) {
-                $scope.info = data;
-                for (var name in data) {
-                    if($scope.info[name] == "null"){
-                        $scope.info[name] = "";
-                    }
-
-                    if ($scope.metadata[name] != null) {
-                        var currentMetaData = $scope.metadata[name]
-                        for (var fieldMetadata in currentMetaData) {
-                            if (currentMetaData[fieldMetadata]["value"] == $scope.info[name]) {
-                                $scope.info[name] = currentMetaData[fieldMetadata];
-                                $scope.selected[name] = currentMetaData[fieldMetadata];
-                                break;
-                            }
-                        }
-                    }
-                }
-
-            });
-
-
-        }else{
-            $scope.info = {photoUrl:"http://www.placehold.it/200x150/EFEFEF/AAAAAA&amp;text=no+image"};
-            $scope.infoId = null;
-        }
-
-        ComponentsPickers.init();
-        ComponentsFormTools.init();
-    });
-
-    /**
-     *  set the save method
-     */
-    $scope.save = function () {
-        for (var name in $scope.info) {
-            //if ($scope.info[key] == null) {
-            //    continue;
-            //}
-            //var value = $scope.info[key].value
-            //if (value !== undefined && value !== null) {
-            //    $scope.info[key] = value;
-            //}
-
-            if(metadata[name] != null){
-                $scope.info[name] = $scope.selected[name].value;
-            }
-        }
-
-        var params = JSON.stringify($scope.info).replace("{", "").replace("}", "").replace(/\,/g, "&").replace(/\"/g, "").replace(/\:/g, "=");
-        $http({
-            method: "post",
-            url: Metronic.rootPath() + "/index.php?s=/addon/School/Admin/saveModel/model/" + $scope.model,
-            data: params,
-            headers: {
-                'Content-Type': 'application/x-www-form-urlencoded'
-            }
-        }).success(function (data) {
-            if (data.status == "1") {
-                $scope.infoErrorShow = false;
-                $scope.infoErrorMsg = "";
-                TableAjax.reload();
-                $("#form_info").modal("hide");
-            } else {
-                $scope.infoErrorShow = true;
-                $scope.infoErrorMsg = data.msg;
-            }
-        });
-    };
-
-    /**
-     * upload the img
-     */
-    $scope.file;
-    $scope.uploadPic = function (ele) {
-        var xhr = new XMLHttpRequest();
-        var files = ele.files;
-        var formData = new FormData();
-        formData.append('filename', files[0]); // index 为第 n 个文件的索引
-        xhr.open('post', Metronic.rootPath() + "/index.php?s=/home/File/uploadPicture.html"); // url 为提交的后台地址
-        xhr.addEventListener("load", uploadComplete, false); // 处理上传完成
-        // 改变t2的函数
-        function uploadComplete() {
-            var returnData = JSON.parse(xhr.responseText);
-            $scope.info[this.id] = returnData.id;
-        }
-
-        xhr.send(formData);
-    }
 }]);
--- admin/js/scripts/weixin-table-ajax.js
+++ admin/js/scripts/weixin-table-ajax.js
@@ -1,28 +1,75 @@
 var TableAjax = function () {
-    var columns = new  Array();
-    var initPickers = function () {
-        //init date pickers
-        $('.date-picker').datepicker({
-            rtl: Metronic.isRTL(),
-            autoclose: true
-        });
-    };
-
-    var init = function (model) {
-        columns = new  Array();
-        $.get(Metronic.rootPath() + "/index.php?s=/addon/School/Admin/getModelFields/model/" + model + "/token/gh_36a5c6958de0", function (data) {
-            columns.push({"sTitle": '<input type="checkbox" class="group-checkable">', "aTargets": [0],"bSortable": false});
-            for (var i = 0; i < data.fields.length; i++) {
-                columns.push({"sTitle": data.list_grids[i].title, "aTargets": [i+1]});
-            }
-            initPickers();
-            handleRecords(Metronic.rootPath() + "/index.php?s=/addon/School/Admin/getDataList/model/" + model + "/token/gh_36a5c6958de0/search/");
-        });
-    };
-
+    var columns = new Array();
+    var module = null;
+    var handleController = null;
     var grid = "";
+    //
+    //var initPickers = function () {
+    //    //init date pickers
+    //    $('.date-picker').datepicker({
+    //        rtl: Metronic.isRTL(),
+    //        autoclose: true
+    //    });
+    //};
 
-    var handleRecords = function (dataSrc) {
+    /**
+     * init the table
+     * @param module
+     * @param handleController
+     */
+    var init = function (module1, handleController1) {
+        if (grid != "" && grid != null) {
+            grid.getDataTable().destroy();
+        }
+
+        if (columns.length > 0) {
+            columns = new Array();
+        }
+        module = module1;
+        handleController = handleController1;
+        $.get(Metronic.rootPath() + "/index.php?s=/addon/" + module + "/" + handleController + "/getModelFields", function (data) {
+            // init column
+            for (var i = 0; i < data.fields.length; i++) {
+                if( data.list_grids[i].title.indexOf('checkbox') > 0 ||  data.list_grids[i].title == "操作"){
+                    columns.push({"sTitle": data.list_grids[i].title, "aTargets": [i],"bSortable": false});
+                }
+                columns.push({"sTitle": data.list_grids[i].title, "aTargets": [i]});
+            }
+
+            //initPickers();
+            handleRecords(createUrl());
+        });
+    };
+
+    /**
+     * reload the table
+     * @param searchName
+     * @param serachText
+     */
+    var reload = function () {
+        grid.getDataTable().ajax.url(createUrl()).load();
+        //init(module, handleController);
+    }
+
+    /**
+     * create the  data url
+     * @returns {string}
+     */
+    var createUrl = function () {
+        var searchName = $("#search-name").val();
+        var searchText = $("#search-text").val();
+        var url = Metronic.rootPath() + "/index.php?s=/addon/" + module + "/" + handleController + "/listsAdmin/token/gh_36a5c6958de0"
+        if (searchName != null && searchText != null && searchName.length > 0 && searchText.length > 0) {
+            url += "/" + searchName + "/" + searchText;
+        }
+        return url;
+    }
+
+    /**
+     * get the data and set
+     * @param url
+     */
+    var handleRecords = function (url) {
         grid = new Datatable();
         grid.init({
             src: $("#datatable_ajax"),
@@ -30,26 +77,22 @@ var TableAjax = function () {
                 // execute some code after table records loaded
             },
             onError: function (grid) {
-                // execute some code on network or other general error  
+                // execute some code on network or other general error
             },
             loadingMessage: '读取中...',
-            dataTable: { // here you can define a typical datatable settings from http://datatables.net/usage/options 
-
-                // Uncomment below line("dom" parameter) to fix the dropdown overflow issue in the datatable cells. The default datatable layout
-                // setup uses scrollable div(table-scrollable) with overflow:auto to enable vertical scroll(see: assets/global/scripts/datatable.js). 
-                // So when dropdowns used the scrollable div should be removed. 
-                //"dom": "<'row'<'col-md-8 col-sm-12'pli><'col-md-4 col-sm-12'<'table-group-actions pull-right'>>r>t<'row'<'col-md-8 col-sm-12'pli><'col-md-4 col-sm-12'>>",
-
-                 aoColumnDefs: columns,
+            dataTable: { // here you can define a typical datatable settings from http://datatables.net/usage/options
+                aoColumnDefs: columns,
+                    //"oLanguage": {
+                    //    "sInfoEmpty": "没有数据"
+                    //},
                 "bStateSave": true, // save datatable state(pagination, sort, etc) in cookie.
-
                 "lengthMenu": [
                     [10, 20, 50, 100, 150, -1],
                     [10, 20, 50, 100, 150, "All"] // change per page values here
                 ],
                 "pageLength": 10, // default record count per page
                 "ajax": {
-                    "url": dataSrc, // ajax source
+                    "url": url// ajax source
                 },
                 //scrollY: true,
                 "order": [
@@ -90,9 +133,8 @@ var TableAjax = function () {
 
     return {
         //main function to initiate the module
-        init: function (model) {
-            init(model);
-
+        init: function (module, handleController) {
+            init(module, handleController);
         },
         delete: function (id) {
             grid.setAjaxParam("customActionType", "action");
@@ -100,18 +142,23 @@ var TableAjax = function () {
             grid.setAjaxParam("id", id);
             grid.getDataTable().ajax.reload();
         },
-        add : function(){
+        add: function () {
             $("#form_info_id").val(null);
             $("#form_info_id").trigger("change");
+            ComponentsPickers.init();
+            ComponentsFormTools.init();
             $("#form_info").modal("show");
         },
-        edit: function(id){
+        edit: function (id) {
             $("#form_info_id").val(id);
             $("#form_info_id").trigger("change");
+            ComponentsPickers.init();
+            ComponentsFormTools.init();
             $("#form_info").modal("show");
         },
-        reload: function(){
-            grid.getDataTable().ajax.reload();
+        reload: function () {
+            reload();
         }
+
     };
 }();
