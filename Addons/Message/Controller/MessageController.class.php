0784a2680711006597b7cd11223a23b50b69feb0
wuhanchu <118130760@qq.com>
2015-12-01 22:47:31 +0800

    增加流水。

--- Addons/Message/Controller/MessageController.class.php
+++ Addons/Message/Controller/MessageController.class.php
@@ -7,37 +7,70 @@ namespace Addons\Message\Controller;
  * Class QuestionController
  * @package Addons\Messaget\Controller
  */
-class MessageController extends MessageBaseController{
+class MessageController extends MessageBaseController
+{
     /**
      * 初始化
      */
-    function _initialize() {
-        $this->model = $this->getModel ( 'student_question' );
-        parent::_initialize ();
+    function _initialize()
+    {
+        $this->model = $this->getModel('student_question');
+        parent::_initialize();
     }
 
     /**
      * 增加
      */
-    public function add(){
-        $_POST['openid'] =  get_openid();
-        $_POST['token'] =  get_token();
-        $_POST['time'] =  date('Y-m-d H:i:s');
+    public function add()
+    {
+        $_POST['openid'] = get_openid();
+        $_POST['token'] = get_token();
+        $_POST['time'] = date('Y-m-d H:i:s');
         parent::add();
     }
 
     /**
      * 查询列表
      */
-    public function lists() {
+    public function lists()
+    {
         // get the data
+
         $list_data = $this->_get_model_list($this->model);
         $this->assign($list_data);
 
+
         //设置显示控件
-        $this->assign('add_button','0');
+        $this->assign('add_button', '0');
 
         // display
-        $this->display ();
+        $this->display();
     }
+
+    public function _search_map($model, $fields)
+    {
+        $map = parent::_search_map($model, $fields);
+        if (is_array($map)) {
+            $where_str = "";
+
+            foreach ($map as $mapMame => $mapValue) {
+                if (!empty($where_str)) {
+                    $where_str .= " and ";
+                }
+                $where_str .= $mapMame . ' = "' . $mapValue . '" ';
+            }
+            $map = $where_str;
+        }
+
+        $db_config = D('Common/AddonConfig')->get(_ADDONS);
+        $showAnswer = $db_config['show_no_answer_question'];
+        if (!$showAnswer) {
+            $map .= " and answer is not null";
+        }
+
+        // 返回
+        return $map;
+    }
+
+
 }
