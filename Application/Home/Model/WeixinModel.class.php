48f6444595217e7d67a2e344132208471e78ce52
wuhanchu <118130760@qq.com>
2015-12-12 00:19:29 +0800

    增加学员信息新增划款按钮.

--- Application/Home/Model/WeixinModel.class.php
+++ Application/Home/Model/WeixinModel.class.php
@@ -124,8 +124,7 @@ class WeixinModel extends Model {
 
 		addWeixinLog(' 发送回复消息到微信平台',$str);
 
-		$lines = explode("\n", $str);
-		echo ($lines[1]);
+		echo ($str);
 	}
 	/* 组装xml数据 */
 	public function _data2xml($xml, $data, $item = 'item') {
