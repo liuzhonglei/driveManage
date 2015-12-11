d66961a99649f06072f8cf15cfb32458ad9f7c4e
wuhanchu <118130760@qq.com>
2015-12-12 00:26:05 +0800

    增加学员信息新增划款按钮.

--- Addons/QingQing/Model/WeixinAddonModel.class.php
+++ Addons/QingQing/Model/WeixinAddonModel.class.php
@@ -7,36 +7,7 @@ use Home\Model\WeixinModel;
 /**
  * school的微信模型
  */
-class WeixinAddonModel extends WeixinModel
-{
-    // 关注公众号事件
-    public function subscribe()
-    {
-        return true;
-    }
+class WeixinAddonModel extends WeixinModel {
 
-    // 取消关注公众号事件
-    public function unsubscribe()
-    {
-        return true;
-    }
-
-    // 扫描带参数二维码事件
-    public function scan()
-    {
-        return true;
-    }
-
-    // 上报地理位置事件
-    public function location()
-    {
-        return true;
-    }
-
-    // 自定义菜单事件
-    public function click()
-    {
-        return true;
-    }
 }
         	
