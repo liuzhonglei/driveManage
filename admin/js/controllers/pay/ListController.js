a99fbb34f6aa3cf0a16ea15c14f9df3394f9a607
wuhanchu <whcwuhanchu@gmail.com>
2016-04-20 23:09:18 +0800

    增加备用同步账号.

--- admin/js/controllers/pay/ListController.js
+++ admin/js/controllers/pay/ListController.js
@@ -1,6 +1,26 @@
 MetronicApp.controller('ListController', ['$rootScope', '$http', '$scope', function ($rootScope, $http, $scope) {
     // param
     $scope.rootData = $rootScope.$state.$current.data;
+    $scope.status = "WAIT";
+
+    /**
+     * 设置状态
+     * @type {number}
+     */
+    $scope.setStatus = function (status) {
+        $scope.status = status;
+        $('#list-search-text').val('')
+        $scope.loadTable();
+    }
+
+    /**
+     * 重新加载表哥
+     */
+    $scope.loadTable = function () {
+        $('#condition-modal').modal('hide');
+        TableAjax.emptyModal($rootScope.$state.$current.data.module, $rootScope.$state.$current.data.handleController, {result_code: $scope.status});
+        TableAjax.init('list', $rootScope.$state.$current.data.module, $rootScope.$state.$current.data.handleController, {result_code: $scope.status});
+    }
 
     // init ajax
     $scope.$on('$viewContentLoaded', function () {
@@ -10,7 +30,8 @@ MetronicApp.controller('ListController', ['$rootScope', '$http', '$scope', funct
         // init the table
         setTimeout(function () {
             // 等待DOM回来，黑科技，哈哈哈
-            TableAjax.init('list', $rootScope.$state.$current.data.module, $rootScope.$state.$current.data.handleController, {"operation":0}, null);
+            TableAjax.init('list', $rootScope.$state.$current.data.module, $rootScope.$state.$current.data.handleController, {result_code: $scope.status}, null);
+
         }, 1);
 
     });
