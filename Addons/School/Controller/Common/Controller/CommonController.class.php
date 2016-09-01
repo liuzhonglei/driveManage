f0c04299bb1d582fca5fe13e825d38da1aafb02d
wuhanchu <whcwuhanchu@gmail.com>
2016-09-01 10:02:59 +0800

    修改wx js 签名接口

--- Addons/School/Controller/Common/Controller/CommonController.class.php
+++ Addons/School/Controller/Common/Controller/CommonController.class.php
@@ -176,8 +176,7 @@ class CommonController extends ExtendAddonsController
      */
     public function wxSignPackage()
     {
-        $appinfo = get_token_appinfo();
-
+        $appinfo = get_token_appinfo("gh_94ecad95d624");
         $jssdk = new \JsSdk($appinfo["appid"], $appinfo["secret"]);
         $signPackage = $jssdk->GetSignPackage($_SERVER['HTTP_REFERER']);
         $this->ajaxReturn($signPackage);
