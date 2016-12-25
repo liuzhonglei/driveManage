3fa2d7135b6bb15bafede8443c3a5d29cc2636fc
whcsdtc <whcwuhanchu@gmail.com>
2016-12-25 13:16:25 +0800

    修改个人中心绑定。

--- Addons/School/View/default/Mobile/js/weixin.js
+++ Addons/School/View/default/Mobile/js/weixin.js
@@ -1,7 +1,7 @@
 /**
  * the call pay method
  */
-function callpay(payParameter,callback) {
+function callpay(payParameter, callback) {
     if (typeof WeixinJSBridge == "undefined") {
         if (document.addEventListener) {
             document.addEventListener('WeixinJSBridgeReady', jsApiCall, false);
@@ -10,22 +10,47 @@ function callpay(payParameter,callback) {
             document.attachEvent('onWeixinJSBridgeReady', jsApiCall);
         }
     } else {
-        jsApiCall(payParameter,callback);
+        jsApiCall(payParameter, callback);
     }
 }
-function jsApiCall(payParameter,callback) {
+function jsApiCall(payParameter, callback) {
     WeixinJSBridge.invoke(
         'getBrandWCPayRequest',
         JSON.parse(payParameter),
         function (res) {
             WeixinJSBridge.log(res.err_msg);
-            if(res.err_msg == "get_brand_wcpay_request:ok" ) {
-                if(callback != null){
+            if (res.err_msg == "get_brand_wcpay_request:ok") {
+                if (callback != null) {
                     callback();
                 }
-            }else if(res.err_msg == "get_brand_wcpay_request:fail" ){
+            } else if (res.err_msg == "get_brand_wcpay_request:fail") {
                 alert("支付失败");
             }
         }
     );
+}
+
+
+function updateUrl(url, key) {
+    var key = (key || 't') + '=';  //默认是"t"
+    var reg = new RegExp(key + '\\d+');  //正则：t=1472286066028
+    var timestamp = +new Date();
+    if (url.indexOf(key) > -1) { //有时间戳，直接更新
+        return url.replace(reg, key + timestamp);
+    } else {  //没有时间戳，加上时间戳
+        if (url.indexOf('\?') > -1) {
+            var urlArr = url.split('\?');
+            if (urlArr[1]) {
+                return urlArr[0] + '?' + key + timestamp + '&' + urlArr[1];
+            } else {
+                return urlArr[0] + '?' + key + timestamp;
+            }
+        } else {
+            if (url.indexOf('#') > -1) {
+                return url.split('#')[0] + '?' + key + timestamp + location.hash;
+            } else {
+                return url + '?' + key + timestamp;
+            }
+        }
+    }
 }
